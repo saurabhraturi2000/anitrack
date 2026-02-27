17ef6ec1e0f12ebc4154759dcb81f1a53cfb641a
Saurabh Raturi <saurabh.raturi.999@gmail.com>
2026-02-27 14:00:07 +0530

    chore(app): capitalize display name to Anitrack

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends ConsumerWidget {
 
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      title: 'anitrack',
+      title: 'Anitrack',
       theme: AppThemeFactory.buildTheme(
         style: appearance.style,
         brightness: Brightness.light,
